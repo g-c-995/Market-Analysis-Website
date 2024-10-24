a7393b6bf53894507ae58e98ee7ca9308f09ff76
MananSyal <158842811+MananSyal@users.noreply.github.com>
2024-10-24 09:40:16 +0530

    Create contactUS.js
    makes the contact page functional

---
+++ contactUS.js
@@ -0,0 +1,17 @@
+new WOW().init();
+
+localStorage.clear();
+console.log(localStorage);
+
+$(document).on("click", "button", function (event) {
+  $(".userInfo").each(function () {
+    var id = $(this).attr("name");
+    var value = $(this).val();
+    if (value === "") {
+      return;
+    }
+    localStorage.setItem(id, value);
+  });
+  console.log(localStorage);
+
+});
